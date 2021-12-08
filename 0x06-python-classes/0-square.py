d669cc047581bfc3abc055964f1eb6c9ad2bd498
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2021-12-08 20:31:37 +0300

    Create 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 0-square.py
+Contains empty class Square
+"""
+
+
+class Square:
+    """An empty class Square
+    This is an empty class to demonstrate how empty classes look like
+    """
