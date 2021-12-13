5ec06726bc4ecb9518c6f8670170cada74067dd6
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2021-12-13 21:19:47 +0300

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module containing empty class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty class rectangle.
+    To be built upon in the subsequent modules.
+    """
+    pass
