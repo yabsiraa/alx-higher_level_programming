af34da5e3830d8d6ef666e4f3137d5758c95548a
yabsiraa <88306942+yabsiraa@users.noreply.github.com>
2022-03-02 18:15:43 -0800

    Create 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+
+let num = parseInt(argv[2]);
+
+if (isNaN(num)) { num = 0; }
+
+function factorial (n) {
+  if (n === 0) { return 1; }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num))
